d0131dc421126412d8ff30dc5de1f658d82d445e
marcelllysander <95347359+marcelllysander@users.noreply.github.com>
2023-11-07 10:10:53 +0700

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ const HOST = '0.0.0.0'
 
 const app = express()
 app.get('/', (req, res) => {
-    res.send('Hello world!\n')
+    res.send('Nama    : Marcell Lysander Ferdian\n')
+    res.send('NIM     : 51022011\n')
+    res.send('Jurusan : Sistem Informasi\n')
 })
 
 app.listen(PORT, HOST)
